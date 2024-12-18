c72eaefb0b2a13b7f45103f3b1f4abcf74a0a484
Alexander T. <167206272+Cyb3rknight@users.noreply.github.com>
2024-12-17 23:47:08 -0500

    FCT Initial commit

---
+++ Mathematical Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿// Prompt the user to enter the first number
+Console.WriteLine("Nombre 1 :");
+
+// Read the user's input and store it in the num variable
+int num = int.Parse(Console.ReadLine());
+
+// Prompt the user to enter the second number
+Console.WriteLine("Nombre 2 :");
+
+// Read the user's input and store it in the num2 variable
+int num2 = int.Parse(Console.ReadLine());
+
+// Prompt the user to enter the third number
+Console.WriteLine("Nombre 3 :");
+
+// Read the user's input and store it in the num3 variable
+int num3 = int.Parse(Console.ReadLine());
+
+// Calculate the sum of the three numbers and display the result
+Console.WriteLine($"La somme de ses chiffres est : {num+num2+num3}");
+
+// Calculate the difference between the first and second numbers and display the result
+Console.WriteLine($"La soustraction du premier et deuxième chiffre : {num-num2}");
+
+// Calculate the product of the first and second numbers and display the result
+Console.WriteLine($"La multiplication du premier et deuxième chiffre : {num*num2}");
+
+// Calculate the division of 20 by 5 and display the result
+Console.WriteLine($"La division de 20 par 5 : {20/5}");
